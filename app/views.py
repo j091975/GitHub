4254c052e942664d4c380ee25167600f2eaf8495
j091975@hotmail.com <j091975@hotmail.com>
2024-07-09 23:50:42 +0100

    base

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
+import os
 from django.shortcuts import render, redirect
+from django.utils.translation import gettext as _
 from django.http import HttpResponse
 from django.db import connection
 from .models import product, SoccerPlayer, Slide
@@ -9,9 +11,25 @@ from django.views.generic import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-
+import pandas as pd
 from django.views import View
+from sklearn.datasets import load_iris
+from sklearn.datasets import make_moons
+from sklearn.datasets import fetch_california_housing
+import matplotlib.pyplot as plt
+from django.conf import settings
+import seaborn as sns
 
+# Step 3: Data Preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+# Step 4: Model Training and Evaluation
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+# Step 5: Model Interpretation and Insights
+import numpy as np
+# Step 6: Further Model Tuning (Optional)
+from sklearn.ensemble import RandomForestRegressor
 
 def generate_random_location():
     # Randomly choose one of 'AND', 'BAS', 'SAL'
@@ -127,3 +145,164 @@ def search_players(request):
     query = request.GET.get('q', '')
     results = SoccerPlayer.objects.filter(name__icontains=query) if query else []
     return render(request, 'app/search.html', {'query': query, 'results': results})
+
+def data_page(request):
+    return render(request, 'app/data_page.html')    
+    
+    # Load the Iris dataset
+#    iris = load_iris()
+#    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+#    df['species'] = iris.target_names[iris.target]
+#
+#    # Example: Data cleansing and documenting
+#    # Assuming some data cleaning steps, e.g., removing duplicates
+#    cleaned_data = df.drop_duplicates()
+#
+#    # Example: Aggregating data
+#    avg_measurements = cleaned_data.groupby('species').mean()
+#    
+#    # Generate the moons dataset
+#    X, y = make_moons(n_samples=300, noise=0.2, random_state=42)
+#
+#    # Convert to DataFrame for better handling (optional)
+#    df = pd.DataFrame(X, columns=['Feature 1', 'Feature 2'])
+#    df['Target'] = y
+#    
+#    # Plot the moons dataset
+#    plt.figure(figsize=(10, 6))
+#    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.viridis, edgecolors='k')
+#    plt.title('Moons Dataset')
+#    plt.xlabel('Feature 1')
+#    plt.ylabel('Feature 2')
+#    # Define the path to save the plot
+#    plot_path = os.path.join(settings.STATICFILES_DIRS[0], 'moons_plot.png')
+#    plt.savefig(plot_path)
+#    plt.close()  # Close the plot to free up memory
+#    
+#    # Load the dataset
+#    california_housing = fetch_california_housing()
+#
+#    # Create a DataFrame from the dataset
+#    data = pd.DataFrame(california_housing.data, columns=california_housing.feature_names)
+#    data['MedHouseVal'] = california_housing.target  # Add the target variable
+#        
+#    # Correlation matrix
+#    corr_matrix = data.corr()
+#    plt.figure(figsize=(10, 8))
+#    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+#    plt.title('Correlation Matrix')
+#    plot_path = os.path.join(settings.STATICFILES_DIRS[0], 'Correlation_Matrix.png')
+#    plt.savefig(plot_path)
+#    plt.close()  # Close the plot to free up memory
+#
+#    # Pair plot
+#    sns.pairplot(data, vars=['MedInc', 'AveRooms', 'AveOccup', 'MedHouseVal'])
+#    plot_path = os.path.join(settings.STATICFILES_DIRS[0], 'pairplot.png')
+#    plt.savefig(plot_path)
+#    plt.close()  # Close the plot to free up memory
+#
+#    # Distribution of the target variable
+#    plt.figure(figsize=(8, 6))
+#    sns.histplot(data['MedHouseVal'], bins=30, kde=True)
+#    plt.title('Distribution of Median House Value')
+#    plot_path = os.path.join(settings.STATICFILES_DIRS[0], 'Distribution_of_Median_House_Value.png')
+#    plt.savefig(plot_path)
+#    plt.close()  # Close the plot to free up memory
+#
+#    # Step 3: Data Preprocessing
+#
+#    # Split the data into features and target
+#    X = data.drop('MedHouseVal', axis=1)
+#    y = data['MedHouseVal']
+#
+#    # Split into training and testing sets
+#    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#
+#    # Standardize the feature data
+#    scaler = StandardScaler()
+#    X_train_scaled = scaler.fit_transform(X_train)
+#    X_test_scaled = scaler.transform(X_test)
+#    
+#    # Step 4: Model Training and Evaluation
+#    
+#    # Initialize and train the model
+#    model = LinearRegression()
+#    model.fit(X_train_scaled, y_train)
+#
+#    # Predict on the test set
+#    y_pred = model.predict(X_test_scaled)
+#
+#    # Evaluate the model
+#    mse = mean_squared_error(y_test, y_pred)
+#    r2 = r2_score(y_test, y_pred)
+#
+#    #print(f'Mean Squared Error: {mse:.4f}')
+#    Mean_Squared_Error = _('Mean Squared Error: {mse:.4f}').format(mse=mse)
+#    #print(f'R^2 Score: {r2:.4f}')
+#    R2_Score = _('R^2 Score: {r2:.4f}').format(r2=r2)
+#
+#    # Step 5: Model Interpretation and Insights
+#
+#    # Coefficients and their corresponding features
+#    coefficients = pd.DataFrame({
+#        'Feature': X.columns,
+#        'Coefficient': model.coef_
+#    })
+#
+#    #print(coefficients.sort_values(by='Coefficient', ascending=False))
+#    coeffic = coefficients.sort_values(by='Coefficient', ascending=False)
+#    # Convert to JSON format
+#    coeffic_sorted_df_json = coeffic.to_json(orient='records')
+#
+#    # Step 6: Further Model Tuning (Optional)
+#
+#
+#    # Initialize and train a Random Forest model
+#    rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
+#    rf_model.fit(X_train, y_train)
+#
+#    # Predict on the test set
+#    rf_y_pred = rf_model.predict(X_test)
+#
+#    # Evaluate the Random Forest model
+#    rf_mse = mean_squared_error(y_test, rf_y_pred)
+#    rf_r2 = r2_score(y_test, rf_y_pred)
+#
+#    #print(f'Random Forest Mean Squared Error: {rf_mse:.4f}')
+#    Random_Forest_Mean_Squared_Error = _('Random Forest Mean Squared Error: {rf_mse:.4f}').format(rf_mse=rf_mse)
+#    #print(f'Random Forest R^2 Score: {rf_r2:.4f}')
+#    Random_Forest_R2_Score = _('Random Forest R^2 Score: {rf_r2:.4f}').format(rf_r2=rf_r2)
+#
+#    # Feature Importance from Random Forest
+#    rf_feature_importance = pd.DataFrame({
+#        'Feature': X.columns,
+#        'Importance': rf_model.feature_importances_
+#    })
+#
+#    #print(rf_feature_importance.sort_values(by='Importance', ascending=False))
+#    featureimportance = ''#_(rf_feature_importance.sort_values(by='Importance', ascending=False))
+#
+#    # Visualizing Feature Importances
+#    plt.figure(figsize=(10, 6))
+#    sns.barplot(x='Importance', y='Feature', data=rf_feature_importance.sort_values(by='Importance', ascending=False))
+#    plt.title('Feature Importance from Random Forest')
+#    plot_path = os.path.join(settings.STATICFILES_DIRS[0], 'Feature_Importance_from_Random_Forest.png')
+#    plt.savefig(plot_path)
+#    plt.close()  # Close the plot to free up memory
+#    
+#    
+#
+#    context = {
+#        'data_summary': cleaned_data.head(),  # Summary of cleaned data
+#        'aggregated_data': avg_measurements.to_html(),  # Display aggregated data
+#        'moons_plot_url': 'moons_plot.png', 
+#        'cal_housing' : data.head(25),
+#        'Correlation_Matrix_url':'Correlation_Matrix.png',
+#        'Distribution_of_Median_House_Value_url':'Distribution_of_Median_House_Value.png',
+#        'Feature_Importance_from_Random_Forest_url' : 'Feature_Importance_from_Random_Forest.png',
+#        'coeffic_sorted_df_json_sorted':coeffic_sorted_df_json,
+#        'Random_Forest_Mean_Squared_Error_txt' : Random_Forest_Mean_Squared_Error,
+#        'Random_Forest_R2_Score_txt' : Random_Forest_R2_Score,
+#        'featureimportance_txt' : featureimportance,
+#    }
+#    return render(request, 'app/data_page.html')#, context)
